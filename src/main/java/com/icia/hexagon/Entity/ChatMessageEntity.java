b82bc95fa01ed2d70f17f0768e5255378c4d6c97
GalaxyOpen <keehong11@naver.com>
2023-06-29 14:04:15 +0900

    2023.06.29 13:10 초기세팅

--- src/main/java/com/icia/hexagon/Entity/ChatMessageEntity.java
+++ src/main/java/com/icia/hexagon/Entity/ChatMessageEntity.java
@@ -1,4 +1,27 @@
 package com.icia.hexagon.Entity;
 
-public class ChatMessageEntity {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@Table(name="chat_table")
+public class ChatMessageEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(length=20, nullable = false)
+    private String chatWriter;
+
+    @Column(length=1000, nullable = false)
+    private String chatContents;
+
+    @ManyToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name="roomId")
+    private ChatRoomEntity chatRoomEntity;
 }
--- src/main/java/com/icia/hexagon/Entity/MemberPhotoEntity.java
+++ src/main/java/com/icia/hexagon/Entity/MemberPhotoEntity.java
@@ -1,4 +1,30 @@
 package com.icia.hexagon.Entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name="member_photo_table")
 public class MemberPhotoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column(length = 100)
+    private String OriginalFileName;
+
+    @Column(length = 100)
+    private String StoredFileName;
+
+    @Column
+    private Long memberId;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="memberId")
+    private MemberEntity memberEntity;
 }

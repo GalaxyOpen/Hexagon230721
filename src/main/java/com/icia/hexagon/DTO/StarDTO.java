be73b61e34d151470d04f81058851cf249bd21bf
mjlee0509 <mjlee0509@gmail.com>
2023-06-29 15:03:57 +0900

    mjlee0509/ DTO 클래스 생성 - Game - GameFile - GameReview - Star

---
+++ src/main/java/com/icia/hexagon/DTO/StarDTO.java
@@ -0,0 +1,15 @@
+package com.icia.hexagon.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class StarDTO {
+    private Long id;
+    private Long memberId;
+    private Long gameId;
+
+
+}

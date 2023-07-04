4755ff92d635a512b06425d7deaeb39119b17e48
Zoum7 <sk50223@naver.com>
2023-07-04 15:18:03 +0900

    3. Security 변경
       [변경사항]
       1. LoginIdPwValidator
          a. session.setAttribute
              name : loginEmail -> loginId 이름 변경
          b. loadUserByUsername(insertedId -> memberId) 이름 변경
          c. memberId -> memberEntity 이름 변경

--- src/main/java/com/icia/hexagon/Config/Security/LoginIdPwValidator.java
+++ src/main/java/com/icia/hexagon/Config/Security/LoginIdPwValidator.java
@@ -25,29 +25,29 @@ public class LoginIdPwValidator implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String insertedId) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
         // 사용자 이메일로 MemberEntity 조회
-        Optional<MemberEntity> memberId = memberRepository.findByMemberId(insertedId);
+        Optional<MemberEntity> byMemberId = memberRepository.findByMemberId(memberId);
 
         // 조회된 MemberEntity가 없을 경우 UsernameNotFoundException 발생
-        if (memberId == null || !memberId.isPresent()) {
+        if (memberId == null || !byMemberId.isPresent()) {
             throw new UsernameNotFoundException("User not found");
         }
 
         // 조회된 MemberEntity에서 비밀번호와 권한 정보 추출
-        String pw = memberId.get().getMemberPassword();
-        String roles = memberId.get().getRoles();
+        String pw = byMemberId.get().getMemberPassword();
+        String roles = byMemberId.get().getRoles();
 
         // UserDetails 객체 생성하여 반환
         UserDetails user = User.builder()
-                .username(insertedId) // 사용자 이메일을 username으로 설정
+                .username(memberId) // 사용자 이메일을 username으로 설정
                 .password(pw) // 조회된 비밀번호를 설정
                 .roles(roles) // 조회된 권한 정보를 설정
                 .build();
 
         // 인증에 성공한 경우 세션에 memberId 값을 저장
         HttpSession session = request.getSession();
-        session.setAttribute("loginId", insertedId);
+        session.setAttribute("loginId", memberId);
 
         return user;
     }
